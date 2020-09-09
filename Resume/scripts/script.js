2a191416512a2f2213cb6cfa334199ca53a2f501
Mitch <mitchellmigala@gmail.com>
2020-09-08 22:04:52 -0500

    just added first js slightly modded index

---
+++ Resume/scripts/script.js
@@ -0,0 +1,31 @@
+//alert("Well well well, Mitcell the file is in fact linked!! Good job!");
+
+//alert("The first paragraph contains " + document.getElementById("yo").innerHTML);
+
+//document.getElementById("yo").innerHTML = "yo! gabba gabba" ;
+
+//alert("Uh oh! Now it contains " + document.getElementById("yo").innerHTML);
+//document.write("<p>It goes something like this. </p>");
+
+
+//const name = prompt("Please enter your name fool!");
+
+const html = "";
+
+
+function writingHtml(html){
+
+    do {
+html = prompt("please enter what you would like to add to the page: when you are done type exit");
+document.write(html);
+console.log("html contains " + html);
+    }
+    while(html !== "exit") ;
+}
+
+
+
+console.log("Well thats it.  Your Javascript has all ran out!");
+console.log("By the way dudes name was " + name);
+
+console.log("By the way html contains " + name);
